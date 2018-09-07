98a1fae721c3dc51bf8e98357c5d687b6aab7ec1
tereophonic <Stereophonic@admindeimac-38.local>
2018-09-07 16:44:37 +0800

    Initial Commit

---
+++ Learn Dic/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Learn Dic
+//
+//  Created by $tereophonic on 18/9/7.
+//  Copyright © 2018年 $tereophonic. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
